b59841a7f2f99c1a3badffe55a921ce666d4d1f6
4l1onGit <hassanali2562@gmail.com>
2024-12-24 16:54:40 +0000

    Implement pagination for logs

--- client/src/Components/LogList.component.tsx
+++ client/src/Components/LogList.component.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { IoMdArrowDropdown, IoMdClose } from "react-icons/io";
 import { Log } from "../utils/util";
 
@@ -11,17 +12,41 @@ interface Props {
 const LogList = ({ toggle, setState }: Props) => {
   const [logs, setLogs] = useState<Log[]>([]);
   const [serverStatus, setServerStatus] = useState<boolean>(false);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     try {
-      axios.get(`${import.meta.env.VITE_URL}/log`).then((res) => {
-        setLogs(res.data);
-        setServerStatus(true);
+      axios.get(`${import.meta.env.VITE_URL}/logTotal`).then((res) => {
+        setTotalPages(Math.ceil(res.data / 5));
       });
     } catch (error) {
       console.log(error);
     }
-  }, [toggle]);
+  }, []);
+
+  useEffect(() => {
+    try {
+      if (page < 1) {
+        setPage(1);
+      }
+      if (page > totalPages) {
+        setPage(totalPages);
+      }
+      axios.get(`${import.meta.env.VITE_URL}/log/${page}`).then((res) => {
+        setLogs(res.data);
+        setServerStatus(true);
+        console.log(page);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [page]);
+
+  const pagination = new Array(totalPages);
+  for (let i = 0; i < pagination.length; i++) {
+    pagination[i] = i;
+  }
 
   return (
     <div
@@ -39,13 +64,52 @@ const LogList = ({ toggle, setState }: Props) => {
         <button onClick={() => setState(false)}>
           <IoMdClose />
         </button>
+
+        <ul className="flex justify-center w-full items-center h-12 space-x-1">
+          {page > 1 ? (
+            <li className="flex items-center">
+              <button
+                className="text-[#FFDFBF] text-4xl "
+                onClick={() => setPage(page - 1)}
+              >
+                <FaChevronLeft />
+              </button>
+            </li>
+          ) : (
+            ""
+          )}
+
+          {pagination.map((_, index) => (
+            <li key={index}>
+              <button
+                className="px-3 border-2 rounded-md border-customblue bg-[#FFDFBF] text-2xl text-customblue-950"
+                onClick={() => setPage(index + 1)}
+              >
+                {index + 1}
+              </button>
+            </li>
+          ))}
+          {page < totalPages ? (
+            <li className="flex items-center">
+              <button
+                className="text-[#FFDFBF] text-4xl"
+                onClick={() => setPage(page + 1)}
+              >
+                <FaChevronRight />
+              </button>
+            </li>
+          ) : (
+            ""
+          )}
+        </ul>
+
         <div className="flex flex-col justify-center w-full h-full items-center">
           <ul className="w-full space-y-2">
             {serverStatus ? (
-              logs.map((log) => (
+              logs.map((log, index) => (
                 <li
                   className="border-2 rounded-2xl border-customblue-600 bg-gradient-to-l from-customblue-800 to-customblue-900 w-full h-22"
-                  key={log.date}
+                  key={`${log.date}${log.glucose}${index}`}
                 >
                   <div className="flex p-2 space-x-2 justify-between">
                     <ul className="flex flex-col text-[#FFDFBF] font-semibold uppercase text-sm">
