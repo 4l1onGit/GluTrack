388e079ce4f86834c68d17e9de39d7c3f0991e3e
4l1onGit <hassanali2562@gmail.com>
2025-02-05 12:54:04 +0000

    implement pwa support

---
+++ client/manifest.ts
@@ -0,0 +1,26 @@
+export const manifest = {
+    
+      name: 'GluTrack',
+      short_name: 'GluTrack',
+      description: 'A Sugar and Carbs tracking app for all platforms',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+            src: '/apple-touch-icon.png',
+            sizes:'180x180',
+            type:'image/png',
+            purpose:'apple touch icon',
+          },
+          
+      ]
+    }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,18 @@
 
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+import { manifest } from './manifest';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(
+    {
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: manifest 
+    }
+    )],
   test: {
     globals: true,
     environment: 'jsdom',
